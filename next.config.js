6dac7e578c65b41896802d034102f7e21283bd3d
oleg.bondarenko <mnkrp.ml@gmail.com>
2023-10-02 19:30:59 +0300

    env setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const repoName = process.env.REPO_NAME;
+
+if (!repoName) { throw new Error('Failed to inject env variable') }
+
 const nextConfig = {
     output: 'export',
+    basePath: `/${repoName}`,
+    assetPrefix: `/${repoName}/`,
 }
 
 module.exports = nextConfig
