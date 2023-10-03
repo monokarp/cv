9e29080d922e4d63aeacbd17b6416f92cf406470
oleg.bondarenko <mnkrp.ml@gmail.com>
2023-10-03 19:13:11 +0300

    exclude base path from config when starting dev server

--- next.config.js
+++ next.config.js
@@ -2,12 +2,13 @@
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
 const repoName = process.env.REPO_NAME;
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
     output: 'export',
 }
 
-if (isGithubActions) {
+if (isGithubActions && isProd) {
     Object.assign(nextConfig, {
         basePath: `/${repoName}`,
     })
