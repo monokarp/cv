b3cf61e2814e00f70680b612a5081c239c4aea82
oleg.bondarenko <mnkrp.ml@gmail.com>
2023-10-07 16:01:09 +0300

    adjusted lg pdf format

--- app/components/pdf-button/pdf-button.tsx
+++ app/components/pdf-button/pdf-button.tsx
@@ -13,7 +13,7 @@ export default function PdfButton() {
                 },
                 jsPDF: {
                     orientation: 'portrait',
-                    format: window.innerWidth <= 1024 ? [210, 578] : [420, 509],
+                    format: window.innerWidth < 1024 ? [210, 578] : [420, 511],
                 }
             })
             .from(document.body)
