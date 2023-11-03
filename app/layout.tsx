090f14698b80ffd5c1610458c8fe379d326dbc4a
oleg.bondarenko <mnkrp.ml@gmail.com>
2023-11-03 08:46:19 +0200

    og tags test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: 'Bondarenko Oleg CV',
-}
+const title = 'CV Oleg Bondarenko';
 
 export default function RootLayout({
   children,
@@ -15,6 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={title} key="ogtitle" />
+        <meta property="og:description" content="A brief summary of my professional career in SE" key="ogdesc" />
+        <meta property="og:site_name" content={title} key="ogsn" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
