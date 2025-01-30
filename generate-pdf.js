47811a8d9cbd394309dc805b9260716c5c8c016d
oleg.bondarenko <mnkrp.ml@gmail.com>
2025-01-30 19:41:37 +0100

    updated rel path

--- generate-pdf.js
+++ generate-pdf.js
@@ -3,12 +3,10 @@ const path = require("path");
 const fs = require("fs").promises;
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repoName = process.env.REPO_NAME;
-const isProd = process.env.NODE_ENV === "production";
+const repoName = process.env.REPO_NAME || 'cv';
 
 console.log("isGithubActions", isGithubActions);
 console.log("repoName", repoName);
-console.log("isProd", isProd);
 
 const pageIndexPath = path.resolve("./out/index.html");
 
@@ -17,11 +15,7 @@ async function replaceInStaticPage(v1, v2) {
   console.log(`Replacing "${v1}" with "${v2}"`);
   const content = await fs.readFile(pageIndexPath, "utf-8");
 
-  console.log(content);
-
   const modified = content.replace(new RegExp(v1, "g"), v2);
-
-  console.log(modified);
 
   await fs.writeFile(pageIndexPath, modified, "utf8");
 }
@@ -45,9 +39,8 @@ async function runBrowserAndSavePDF() {
   await browser.close();
 }
 
-const repoPrefix = isGithubActions && isProd ? repoName : "";
-const originalPrefix = isGithubActions ? `/cv/_next/` : "/_next/";
-const relativePrefix = isGithubActions ? `./cv/_next/` : "./_next/";
+const originalPrefix = isGithubActions ? `/${repoName}/_next/` : "/_next/";
+const relativePrefix = `./_next/`;
 
 (async () => {
   await replaceInStaticPage(originalPrefix, relativePrefix);
