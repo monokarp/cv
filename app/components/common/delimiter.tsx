603d3e38b20d1ce3ba9ed70d3dafff6f047cacc3
oleg.bondarenko <mnkrp.ml@gmail.com>
2025-01-30 11:52:45 +0100

    removed pdf generation, replaced it with browser print

--- app/components/common/delimiter.tsx
+++ app/components/common/delimiter.tsx
@@ -1,6 +1,6 @@
 
 export default function Delimiter() {
   return (
-    <div className="h-[1px] w-full border-solid border-b-[1px] border-gray-300 mb-2"></div>
+    <div className="h-[1px] w-full border-solid border-b-[1px] border-gray-300 my-2"></div>
   );
 }
--- app/components/pdf-button/pdf-button.tsx
+++ app/components/pdf-button/pdf-button.tsx
@@ -1,27 +1,13 @@
-'use client';
+"use client";
 
 export default function PdfButton() {
+  const printPdf = async () => {
+    window.print();
+  };
 
-    const printPdf = async () => {
-        const html2pdf = await import('html2pdf.js');
-
-        html2pdf.default()
-            .set({
-                margin: 0,
-                html2canvas: {
-                    scale: 2,
-                },
-                jsPDF: {
-                    orientation: 'portrait',
-                    format: window.innerWidth < 1024 ? [210, 578] : [420, 588],
-                }
-            })
-            .from(document.body)
-            .toPdf()
-            .save('oleg-bondarenko-cv.pdf');
-    }
-
-    return (
-        <div className="link cursor-pointer font-medium" onClick={printPdf}>(Download PDF)</div>
-    );
+  return (
+    <div className="link cursor-pointer font-medium" onClick={printPdf}>
+      (Download PDF)
+    </div>
+  );
 }
